f4d616894a34c84f1e7bf4ee93d740a04454c322
GongJr0 <guneykiymac@gmail.com>
2024-05-15 12:45:53 +0200

    update

--- frontend.py
+++ frontend.py
@@ -1,6 +1,6 @@
 from backend import Backend
 import customtkinter as ctk # type: ignore
-from PIL import ImageTk, Image # type: ignore
+from PIL import Image # type: ignore
 
 class App(Backend):
     def __init__(self, master: ctk.CTk) -> None:
@@ -22,20 +22,20 @@ class App(Backend):
         self.photo = ctk.CTkImage(self.img)
         self.photo.configure(size=(100, 100))
         
-        self.condition_frame = ctk.CTkFrame(self.master)
-        self.condition_image = ctk.CTkLabel(self.condition_frame, image=self.photo, text='')
+        self.condition_img_frame = ctk.CTkFrame(self.master)
+        self.condition_image = ctk.CTkLabel(self.condition_img_frame, image=self.photo, text='')
         
+        self.info_frame = ctk.CTkFrame(self.master)
         
-        self.condition_text = ctk.CTkLabel(self.master, text=f"Condition: {self.get_condition()['text']}", font=("Helvetica", 20))
+        self.condition_text = ctk.CTkLabel(self.info_frame, text=f"Condition: {self.get_condition()['text']}", font=("Helvetica", 20))
+        self.temperature = ctk.CTkLabel(self.info_frame, text=f"Temperature: {int(self.get_temperature())}\u2103", font=("Helvetica", 20))
         
-        self.condition_frame.pack()
-        self.condition_image.pack()
-        self.condition_text.pack()
+        self.condition_img_frame.grid(row=0, column=0, padx=5, pady=5)
+        self.condition_image.grid(row=0, column=0)
         
-        
-        # Temperature
-        self.temperature = ctk.CTkLabel(self.master, text=f"Temperature: {int(self.get_temperature())}\u2103", font=("Helvetica", 20))
-        self.temperature.pack()
+        self.info_frame.grid(row=0, column=1, padx=5, pady=5)
+        self.condition_text.grid(row=0, column=0, padx=5, pady=5)
+        self.temperature.grid(row=1, column=0, padx=5, pady=5)
         
         
 
