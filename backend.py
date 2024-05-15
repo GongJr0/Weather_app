5529b56908307ddab0e7c72748aca50f7c0563a5
GongJr0 <guneykiymac@gmail.com>
2024-05-15 19:09:44 +0200

    update

--- backend.py
+++ backend.py
@@ -2,6 +2,9 @@ import requests
 import json
 import geocoder # type: ignore
 import io
+import datetime as dt # type: ignore
+
+from typing import List
 
 class Backend:
     
@@ -13,7 +16,10 @@ class Backend:
             self.api_key = key["API_KEY"]
 
         self.location = self.get_location()
-        
+    
+    def get_image(self, url: str) -> io.BytesIO:
+        return io.BytesIO(requests.get(url).content)
+    
     def get_location(self) -> dict[str, float]:
         g = geocoder.ip('me')
         return {"lat": g.latlng[0], "lon": g.latlng[1]}
@@ -23,8 +29,8 @@ class Backend:
         response = requests.get(url)
         return response.json()
     
-    def get_forecast(self) -> dict:
-        url = f"{self.base_url}/forecast.json?key={self.api_key}&q={self.location['lat']},{self.location['lon']}"
+    def get_forecast(self, days: int = 14) -> dict:
+        url = f"{self.base_url}/forecast.json?key={self.api_key}&q={self.location['lat']},{self.location['lon']}&days={days}"
         response = requests.get(url)
         return response.json()
 
@@ -33,15 +39,36 @@ class Backend:
     
     def get_wind(self) -> float:
         return self.get_current_report()["current"]["wind_kph"]
+    
+    def get_gusts(self) -> float:
+        return self.get_current_report()["current"]["gust_kph"]
 
     def get_condition(self) -> dict:
         return self.get_current_report()["current"]["condition"]
+    
+    def get_humidity(self) -> float:
+        return self.get_current_report()["current"]["humidity"]
+    
+    def get_sunrise(self) -> str:
+        return self.get_forecast()['forecast']['forecastday'][0]['astro']['sunrise']
+    
+    def get_sunset(self) -> str:
+        return self.get_forecast()['forecast']['forecastday'][0]['astro']['sunset']
     
     def get_current_icon(self) -> io.BytesIO:
         url = "https:"+ self.get_condition()["icon"]
         return io.BytesIO(requests.get(url).content)
     
+    def get_14_days_forecast(self) -> dict:
+        today = dt.datetime.now()
+        next_14 = list(map(lambda x: x.strftime(format='%Y-%m-%d'), [today + dt.timedelta(days=i) for i in range(1, 15)]))
+        
+        forecast: List = self.get_forecast()['forecast']['forecastday']
+        return {day: {'maxtemp_c': forecast[i]['day']['maxtemp_c'], 'mintemp_c': forecast[i]['day']['mintemp_c'], 'icon': forecast[i]['day']['condition']['icon']} for i, day in enumerate(next_14)}
+    
 if __name__ == "__main__":
     test = Backend()
-    print(test.get_condition())
+    day = (dt.datetime.now() + dt.timedelta(days=1)).strftime("%Y-%m-%d")
+    print(day)
+    print(test.get_forecast()['location']['name'])
     
--- frontend.py
+++ frontend.py
@@ -1,44 +1,133 @@
 from backend import Backend
+
+import os
 import customtkinter as ctk # type: ignore
-from PIL import Image # type: ignore
+from PIL import Image, ImageTk # type: ignore
+import datetime as dt # type: ignore
 
 class App(Backend):
     def __init__(self, master: ctk.CTk) -> None:
         # Initialize the backend
-        super().__init__()
-        
+        super().__init__()  
         # Initialize and configure the master window
         self.master = master
         self.master.title("Weather App")
-        self.master.iconbitmap("./weather/icon.ico")
         
-        self.master.geometry("800x600")
-        self.master.resizable(False, False)
+        if os.environ.get('OS', '') == 'Windows_NT':
+            self.master.iconbitmap("./weather/icon.ico")
+            
+        else:
+            self.master.iconphoto(False, ImageTk.PhotoImage(Image.open("./weather/icon.png")))
+            
+        self.master.geometry("525x625")
+        self.master.resizable(False, True)
         
+        self.redner_info()
+        self.render_additional_info()
+        self.render_14_day_forecast()
         
-
+    def redner_info(self) -> None:
         # Condition
         self.img = Image.open(self.get_current_icon())
         self.photo = ctk.CTkImage(self.img)
         self.photo.configure(size=(100, 100))
         
-        self.condition_img_frame = ctk.CTkFrame(self.master)
-        self.condition_image = ctk.CTkLabel(self.condition_img_frame, image=self.photo, text='')
+        self.today_title_frame = ctk.CTkFrame(self.master, corner_radius=10)
+        self.today_title_frame.grid(row=0, column=0, padx=5, pady=5, sticky='w')
         
-        self.info_frame = ctk.CTkFrame(self.master)
+        self.today_title = ctk.CTkLabel(self.today_title_frame, text="Today's Forecast", font=("Helvetica", 20))
+        self.today_title.grid(row=0, column=0, padx=5, pady=5)
         
-        self.condition_text = ctk.CTkLabel(self.info_frame, text=f"Condition: {self.get_condition()['text']}", font=("Helvetica", 20))
-        self.temperature = ctk.CTkLabel(self.info_frame, text=f"Temperature: {int(self.get_temperature())}\u2103", font=("Helvetica", 20))
+        self.today_frame = ctk.CTkFrame(self.master, corner_radius=10)
+        self.today_frame.grid(row=1, column=0)
+        
+        self.condition_img_frame = ctk.CTkFrame(self.today_frame, corner_radius=10)
+        self.condition_image = ctk.CTkLabel(self.condition_img_frame, image=self.photo, text='', corner_radius=10)
         
         self.condition_img_frame.grid(row=0, column=0, padx=5, pady=5)
-        self.condition_image.grid(row=0, column=0)
+        self.condition_image.grid(row=0, column=0, padx=5, pady=5)
         
-        self.info_frame.grid(row=0, column=1, padx=5, pady=5)
-        self.condition_text.grid(row=0, column=0, padx=5, pady=5)
-        self.temperature.grid(row=1, column=0, padx=5, pady=5)
+        self.loc_obj = self.get_forecast()['location']
+        self.loc = ctk.CTkLabel(self.condition_img_frame, text=f"{self.loc_obj['name']}, {self.loc_obj['country']}", font=("Helvetica", 10))
+        self.loc.grid(row=1, column=0)
+        
+        self.info_frame = ctk.CTkFrame(self.today_frame, corner_radius=10)
+        self.info_frame.grid(row=0, column=1)
+        
+        self.condition_text = ctk.CTkLabel(self.info_frame, text=f"{self.get_condition()['text']}", font=("Helvetica", 10))
+        self.condition_text.grid(row=0, column=0)
+        
+        self.temperature = ctk.CTkLabel(self.info_frame, text=f"{int(self.get_temperature())}\u2103", font=("Helvetica", 25))
+        self.temperature.grid(row=1, column=0)
+        
+        self.wind = ctk.CTkLabel(self.info_frame, text=f"Current Average Wind Speed: {self.get_wind()} km/h", font=("Helvetica", 10))
+        self.wind.grid(row=2, column=0)
         
         
-
+        
+        return None
+    
+    def render_additional_info(self) -> None:
+        self.additional_info_frame = ctk.CTkFrame(self.today_frame, corner_radius=10)
+        self.additional_info_frame.grid(row=0, column=2, padx=5, pady=5)
+        
+        self.sunrise = ctk.CTkLabel(self.additional_info_frame, text=f"Sunrise: {self.get_sunrise()}", font=("Helvetica", 10), padx=5, pady=5)
+        self.sunrise.grid(row=0, column=0)
+        
+        self.sunset = ctk.CTkLabel(self.additional_info_frame, text=f"Sunset: {self.get_sunset()}", font=("Helvetica", 10), padx=5, pady=5)
+        self.sunset.grid(row=1, column=0)
+        
+        self.humidity = ctk.CTkLabel(self.additional_info_frame, text=f"Humidity: {self.get_humidity()}%", font=("Helvetica", 10), padx=5, pady=5)
+        self.humidity.grid(row=0, column=1, sticky='e')
+        
+        self.gusts = ctk.CTkLabel(self.additional_info_frame, text=f"Gusts: {self.get_gusts()} km/h", font=("Helvetica", 10), padx=5, pady=5)
+        self.gusts.grid(row=1, column=1, sticky='e')
+        
+        return None
+        
+        
+        
+    def render_14_day_forecast(self) -> None:    
+        self.forecast_frame = ctk.CTkFrame(self.master)
+        self.forecast_frame.grid(row=3, column=0, columnspan=7)
+        self.forecasts = self.get_14_days_forecast()
+        
+        self.forecast_title_frame = ctk.CTkFrame(self.master, corner_radius=10)
+        self.forecasts_title = ctk.CTkLabel(self.forecast_title_frame, text="14-Day Forecast", font=("Helvetica", 20))
+        
+        self.forecast_title_frame.grid(row=2, column=0, padx=5, pady=5, sticky='w')
+        self.forecasts_title.grid(row=0, column=0, padx=5, pady=5)
+        
+        
+        
+        for i, (day, forecast) in enumerate(self.forecasts.items()):
+            day_frame = ctk.CTkFrame(self.forecast_frame, corner_radius=10)
+            
+            day_image = Image.open(self.get_image("https:"+ forecast['icon']))
+            day_photo = ctk.CTkImage(day_image)
+            day_photo.configure(size=(50, 50))
+            
+            day_htemp = ctk.CTkLabel(day_frame, text=f"High: {int(forecast['maxtemp_c'])}\u2103", font=("Helvetica", 10))
+            day_ltemp = ctk.CTkLabel(day_frame, text=f"Low: {int(forecast['mintemp_c'])}\u2103", font=("Helvetica", 10))
+            
+            day_image_label = ctk.CTkLabel(day_frame, image=day_photo, text='')
+            
+            day_title = ctk.CTkLabel(day_frame, text=dt.datetime.strptime(day, '%Y-%m-%d').strftime('%b %d'), font=("Helvetica", 15))
+            
+            if i < 7:
+                day_title.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
+                day_frame.grid(row=1, column=i, padx=5, pady=5, sticky='ew')
+                day_image_label.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
+                day_htemp.grid(row=2, column=0, padx=5, pady=5, sticky='ew')
+                day_ltemp.grid(row=3, column=0, padx=5, pady=5, sticky='ew')
+            else :
+                day_title.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
+                day_frame.grid(row=2, column=i-7, padx=5, pady=5, sticky='ew')
+                day_image_label.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
+                day_htemp.grid(row=2, column=0, padx=5, pady=5, sticky='ew')
+                day_ltemp.grid(row=3, column=0, padx=5, pady=5, sticky='ew')
+        
+        return None
         
 def main():
     root = ctk.CTk()
